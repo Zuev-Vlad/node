5a4d92d338f2f4adc3bb9a6ac0d6d2375fd5a752
Z.Vlad <v.zui@yandex.ru>
2021-06-07 20:38:38 +0300

    ывоаыао

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(routerUser)
 // app.use((req, res) => {
 //     res.sendFile(path.join(__dirname, '/', 'public/index.html'))
 // })
-app.use(express.static('/public/index.html'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:' + port)
